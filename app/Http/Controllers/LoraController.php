880cc588cb0d6b1ac446922af1286d841d5ffc88
muxammedqudaynazarov <m.qudaynazarov@gmail.com>
2026-02-21 22:30:53 +0500

    Lora-6

--- app/Http/Controllers/LoraController.php
+++ app/Http/Controllers/LoraController.php
@@ -10,7 +10,7 @@ class LoraController extends Controller
 {
     public function index()
     {
-        $devices = Device::with('datum')->where('status', '1')->get();
+        $devices = Device::with('datum')->orderBy('deviceName')->where('status', '1')->get();
         return response()->json($devices);
     }
 
