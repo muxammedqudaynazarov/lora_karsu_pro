2eebef28f5122753587628b7c180a50f33ec97bd
muxammedqudaynazarov <m.qudaynazarov@gmail.com>
2026-02-21 23:04:13 +0500

    Lora-6

--- resources/views/inform.blade.php
+++ resources/views/inform.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <link rel="icon" href="{{ asset('favicon.ico') }}">
-    <title>Lora Devices Dashboard</title>
+    <title>loraWAN.nmtu.uz</title>
     <style>
         :root {
             --primary: #4a90e2;
